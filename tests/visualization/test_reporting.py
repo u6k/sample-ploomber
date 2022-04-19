ee192efdca23e3df9f4eac0f2376c86774de32a7
u6k <u6k.apps@gmail.com>
2022-04-19 15:20:09 +0900

    Added: 環境設定を分離した

--- tests/visualization/test_reporting.py
+++ tests/visualization/test_reporting.py
@@ -1,8 +1,8 @@
 def test_product():
     # Load file
-    with open("reports/classification_report.txt", "r") as f:
+    with open("/var/output/reports/classification_report.txt", "r") as f:
         cls_report = f.read()
-    with open("reports/confusion_matrix.txt", "r") as f:
+    with open("/var/output/reports/confusion_matrix.txt", "r") as f:
         con_matrix = f.read()
 
     # Assert
