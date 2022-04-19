d0810f5a45efddbe2c6cb9dcd65479cb3c0d0451
u6k <u6k.apps@gmail.com>
2022-04-19 12:56:13 +0900

    Changed: 処理内容や成果物を変更

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,8 @@
-from pathlib import Path
-
 import pandas as pd
 from sklearn.datasets import load_wine
 
 # %% tags=["parameters"]
 upstream = None
-product = {
-    "df_wine": "../../data/raw/df_wine.parquet",
-}
 
 
 # %%
@@ -26,9 +21,4 @@ df_wine
 
 
 # %%
-path_df_wine = Path(product["df_wine"]).resolve()
-path_df_wine
-
-
-# %%
-df_wine.to_parquet(path_df_wine)
+df_wine.to_parquet(product["df_wine"])
--- src/visualization/reporting.py
+++ src/visualization/reporting.py
@@ -2,22 +2,40 @@ import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 
 # %% tags=["parameters"]
-upstream = {
-    "train_model": {
-        "pred_test": "../../data/processed/df_pred_test.parquet",
-    }
-}
-product = None
+upstream = ["train_model"]
 
 
 # %%
-df_pred_test = pd.read_parquet(upstream["train_model"]["pred_test"])
+df_pred_test = pd.read_parquet(upstream["train_model"]["df_pred_test"])
 df_pred_test
 
 
 # %%
-print(classification_report(df_pred_test["target"], df_pred_test["pred"]))
+cls_report = classification_report(df_pred_test["target"], df_pred_test["pred"])
+print(cls_report)
 
 
 # %%
-confusion_matrix(df_pred_test["target"], df_pred_test["pred"])
+matrix_labels = [0, 1, 2]
+
+con_matrix = confusion_matrix(
+    df_pred_test["target"],
+    df_pred_test["pred"],
+    labels=matrix_labels
+)
+
+df_con_matrix = pd.DataFrame(
+    con_matrix,
+    columns=[f"pred_{l}" for l in matrix_labels],
+    index=[f"act_{l}" for l in matrix_labels]
+)
+
+df_con_matrix
+
+
+# %%
+with open(product["classification_report"], "w") as f:
+    f.write(cls_report)
+
+with open(product["confusion_matrix"], "w") as f:
+    f.write(df_con_matrix.to_string())
