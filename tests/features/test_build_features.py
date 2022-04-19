a9d0ab5067c8bec3bcf9cfc904aa52a4090ff795
u6k <u6k.apps@gmail.com>
2022-04-19 12:58:12 +0900

    Added: パイプライン処理の成果物をテストする

---
+++ tests/features/test_build_features.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def test_product():
+    df_wine = pd.read_parquet("data/interim/df_wine.parquet")
+
+    assert len(df_wine) > 0
+    for t in df_wine["target"]:
+        assert t in [0, 1, 2]
