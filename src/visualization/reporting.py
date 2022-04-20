1457c7b116247eaa6e085924682accee334c8bf7
u6k <u6k.apps@gmail.com>
2022-04-21 02:33:50 +0900

    Added: mlflowログ処理を追加した

--- src/visualization/reporting.py
+++ src/visualization/reporting.py
@@ -1,5 +1,8 @@
+import mlflow
 import pandas as pd
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import (accuracy_score, classification_report,
+                             confusion_matrix, f1_score, precision_score,
+                             recall_score)
 
 # %% tags=["parameters"]
 upstream = ["train_model"]
@@ -9,6 +12,17 @@ product = None
 # %%
 df_pred_test = pd.read_parquet(upstream["train_model"]["df_pred_test"])
 df_pred_test
+
+
+# %%
+model_score = {
+    "accuracy": accuracy_score(df_pred_test["target"], df_pred_test["pred"]),
+    "precision": precision_score(df_pred_test["target"], df_pred_test["pred"], average="macro"),
+    "recall": recall_score(df_pred_test["target"], df_pred_test["pred"], average="macro"),
+    "f1": f1_score(df_pred_test["target"], df_pred_test["pred"], average="macro"),
+}
+
+model_score
 
 
 # %%
@@ -40,3 +54,11 @@ with open(product["classification_report"], "w") as f:
 
 with open(product["confusion_matrix"], "w") as f:
     f.write(df_con_matrix.to_string())
+
+
+# %%
+mlflow.set_experiment("sample-ploomber")
+with mlflow.start_run(run_name="reporting"):
+    mlflow.log_metrics(model_score)
+    mlflow.log_artifact(product["classification_report"])
+    mlflow.log_artifact(product["confusion_matrix"])
